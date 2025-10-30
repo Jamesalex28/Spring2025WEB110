885a14505b7930d3b0f26d1fb25e7221ab0f0b47
Jamesalex28 <jlee188@stumail.jccc.edu>
2025-10-30 13:33:08 -0500

    Create script.js

---
+++ Javascript1/script.js
@@ -0,0 +1,23 @@
+/*
+Name: James Lee
+Date: 27/10/25
+Source: Wes Bos JavaScript30 - Drum Kit Project
+Notes: Modified for WEB124 - JCCC
+*/
+
+window.addEventListener("keydown", function (e) {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+  if (!audio) return; // stop if no key
+  audio.currentTime = 0; // rewind
+  audio.play();
+  key.classList.add("playing");
+});
+
+function removeTransition(e) {
+  if (e.propertyName !== "transform") return;
+  this.classList.remove("playing");
+}
+
+const keys = document.querySelectorAll(".key");
+keys.forEach((key) => key.addEventListener("transitionend", removeTransition));
